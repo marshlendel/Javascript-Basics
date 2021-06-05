d5a105660dabedd3cf301f446e1e46ffb3db4e08
Marshall Brown <marshallendel@gmail.com>
2021-06-04 21:29:18 -0400

    booleans baby!

---
+++ JavaScript-Basics/05-booleans.js
@@ -0,0 +1,27 @@
+let isLoggedIn = true;
+let isAuthenticated = true;
+
+console.log(2 =="2");
+
+console.log(2 > 1);
+console.log(3 < 2);
+
+console.log("Password12!" === "Password12!!");
+console.log(1 !== 2);
+console.log(10 !== 10);
+
+//Logical Operators
+//&& and
+//|| or
+//! not
+
+let x = true;
+let y = false;
+
+console.log(x&&y);
+console.log(x || y);
+console.log(!x);
+
+console.log("&& :", 2===3 && 1===1);
+console.log("|| :", 2===1 || 2===1);
+console.log("!=", 2 != "2");
